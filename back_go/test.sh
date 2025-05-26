82190156cbfa6068ba89a66b048bc5f7dc6c7aa0
Mirko Bozzetto <mirkouch@gmail.com>
2025-05-26 21:14:03 +0200

    :fire: chore: remove obsolete test script for Docker setup

--- back_go/test.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-docker-compose down
-docker volume prune -f
-docker-compose up -d
-docker logs bce_postgres
-docker exec bce_postgres psql -U mirkobozzetto -d bce_db -c "SELECT version();"
